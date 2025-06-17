bdf7297cda71b4f9633d3c1209a2ce15dfbe19b7
fazair1 <fauzan.irfanto1@gmail.com>
2025-06-17 18:29:39 +0700

    feat: database setup

---
+++ src/main/java/com/fauzanirfanto/product_manager/model/ProductCategory.java
@@ -0,0 +1,94 @@
+package com.fauzanirfanto.product_manager.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "MstProductCategory")
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDProductCategory")
+    private Long id;
+
+    @Column(name = "Nama", nullable = false, length = 50, unique = true)
+    private String nama;
+
+    @OneToMany(mappedBy = "productCategory", orphanRemoval = false)
+    private List<Product> products;
+
+    @Column(name = "CreatedBy",updatable = false, nullable = false)
+    private Long createdBy = 1L;
+
+    @Column(name = "CreatedDate",updatable = false, nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @Column(name = "ModifiedBy", insertable = false)
+    private Long modifiedBy;
+
+    @Column(name = "ModifiedDate", insertable = false)
+    @UpdateTimestamp
+    private LocalDateTime modifiedDate;
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
