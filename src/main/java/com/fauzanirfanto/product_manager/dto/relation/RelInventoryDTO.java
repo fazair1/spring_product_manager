03af9ef640fc6886211b69c43dd3d58cb3eec863
fazair1 <fauzan.irfanto1@gmail.com>
2025-09-11 16:06:57 +0700

    fix: inventory relation

--- src/main/java/com/fauzanirfanto/product_manager/dto/relation/RelInventoryDTO.java
+++ src/main/java/com/fauzanirfanto/product_manager/dto/relation/RelInventoryDTO.java
@@ -4,6 +4,16 @@ public class RelInventoryDTO {
 
     private Long id;
 
+    private RelProductDTO product;
+
+    public RelProductDTO getProduct() {
+        return product;
+    }
+
+    public void setProduct(RelProductDTO product) {
+        this.product = product;
+    }
+
     public Long getId() {
         return id;
     }
